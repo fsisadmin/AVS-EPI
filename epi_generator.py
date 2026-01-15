08b556810cacabf1ed871c30de50b233d37591e0
fsisadmin <joe.valentine@franklinst.com>
2026-01-15 13:29:38 -0500

    Update import from PyPDF2 to pypdf

--- epi_generator.py
+++ epi_generator.py
@@ -8,7 +8,7 @@ import base64
 from datetime import datetime
 import re
 import requests
-from PyPDF2 import PdfReader, PdfWriter
+from pypdf import PdfReader, PdfWriter
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfbase import pdfmetrics
